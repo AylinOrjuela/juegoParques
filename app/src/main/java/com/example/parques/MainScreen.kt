15e44a4672d71308eca1d82b8bad767dfb625abc
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-10-27 19:09:17 -0500

    Configuracion de firebase y adicion de binding

--- app/src/main/java/com/example/parques/MainScreen.kt
+++ app/src/main/java/com/example/parques/MainScreen.kt
@@ -1,11 +1,29 @@
 package com.example.parques
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.parques.databinding.ActivityMainScreenBinding
 
 class MainScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityMainScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_screen)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
+
     }
+
+    private fun initListeners() {
+        binding.btnRules.setOnClickListener {
+            val intent = Intent(this, RulesActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnCreate.setOnClickListener {
+            val intent = Intent(this, CreateGameActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
--- app/src/main/java/com/example/parques/RulesActivity.kt
+++ app/src/main/java/com/example/parques/RulesActivity.kt
@@ -1,11 +1,20 @@
 package com.example.parques
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.parques.databinding.ActivityRulesBinding
 
 class RulesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRulesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_rules)
+        binding = ActivityRulesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener {
+            val intent = Intent(this,MainScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
