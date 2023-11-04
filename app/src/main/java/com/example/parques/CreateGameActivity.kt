62518d26007c85cb76fa4d2f84e3b736fe29a6d9
AylinOrjuela <acorjuelal@udistrital.edu.co>
2023-11-04 18:56:31 -0500

    Avance conexion en firebase

--- app/src/main/java/com/example/parques/CreateGameActivity.kt
+++ app/src/main/java/com/example/parques/CreateGameActivity.kt
@@ -2,34 +2,44 @@ package com.example.parques
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.parques.databinding.ActivityCreateGameBinding
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
 class CreateGameActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityCreateGameBinding
+    private lateinit var binding: ActivityCreateGameBinding
+    private var id: Int = 0
+
     val db = Firebase.firestore
+    val partida = db.collection("Juego").document("Partida")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        asignarId()
+    }
 
-        val Registro1 = db.collection("Partida").document("Jugador1")
-        val Registro2 = db.collection("Partida").document("Jugador2")
+    private fun asignarId() {
 
-        Registro1.get()
-            .addOnSuccessListener { registro ->
-                if (registro.exists()) {
-                    binding.tvName1.text = "Jugador1"
+        partida.addSnapshotListener { consulta, error ->
+            error?.let {
+                Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            consulta?.let {
+                if (consulta.getBoolean("EstadoJ1") == false) {
+                    id = 1
+                    binding.tvName1.text = "pedro"
+
+                }
+                if (consulta.getBoolean("EstadoJ2") == false) {
+                    id = 2
+                    binding.tvName2.text = "manuel"
                 }
             }
 
-        Registro2.get()
-            .addOnSuccessListener { registro ->
-                if (registro.exists()) {
-                    binding.tvName2.text = "Jugador2"
-                }
-            }
-
+        }
     }
 }
---
+++ app/src/main/java/com/example/parques/partida.kt
@@ -0,0 +1,10 @@
+package com.example.parques
+
+data class partida(
+    var EstadoJ1:Boolean,
+    var EstadoJ2:Boolean,
+    var EstadoPartida:String,
+    var PosJ1:String,
+    var PosJ2:String,
+    var TurnoJugador:Boolean
+    )
