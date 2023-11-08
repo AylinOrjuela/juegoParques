0f8f4dc8b317d36eb9468f4d3f79681dc0a8333c
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-11-07 20:15:07 -0500

    cambios

--- app/src/main/java/com/example/parques/CreateGameActivity.kt
+++ app/src/main/java/com/example/parques/CreateGameActivity.kt
@@ -26,14 +26,16 @@ class CreateGameActivity : AppCompatActivity() {
 
     private fun initListeners(){
         binding.btnPlayer1.setOnClickListener {
-            setUpListeners(1)
+            setPlayer(1)
+            setUpListeners()
         }
         binding.btnPlayer2.setOnClickListener {
-            setUpListeners(2)
+            setPlayer(2)
+            setUpListeners()
         }
     }
 
-    private fun setUpListeners(btn:Int) {
+    private fun setPlayer(btn:Int){//Asignar los jugadores
         val postListener = object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val data = getCleanSnapshot(snapshot)
@@ -44,15 +46,34 @@ class CreateGameActivity : AppCompatActivity() {
                         firebaseInstance.writeOnFirebase(aux)
                         binding.btnPlayer1.isEnabled = false
                         binding.btnPlayer2.isEnabled = false
-                        binding.tvName1.text = "Conectado"
                         id = 1
                     }else if(btn == 2){
                         aux.EstadoJ2=true
                         firebaseInstance.writeOnFirebase(aux)
                         binding.btnPlayer2.isEnabled = false
                         binding.btnPlayer1.isEnabled = false
-                        binding.tvName2.text = "Conectado"
                         id = 2
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.i("Error onCancelled", error.details)
+            }
+        }
+        firebaseInstance.setupDatabaseListener(postListener)
+    }
+
+    private fun setUpListeners() {//Mostrar los jugadores
+        val postListener = object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data = getCleanSnapshot(snapshot)
+                var aux = data?.second
+                if (aux != null) {
+                    if(aux.EstadoJ1 == true){
+                        binding.tvName1.text = "Conectado"
+                    }
+                    if(aux.EstadoJ2 == true){
+                        binding.tvName2.text = "Conectado"
                     }
                 }
             }
