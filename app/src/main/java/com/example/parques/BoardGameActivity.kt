e10dc4884917fcad0b21ab34a54ff63226aceb4a
JuanPCFdev <jpcuellarf@udistrital.edu.co>
2023-11-09 17:55:07 -0500

    Logica de turnos

--- app/src/main/java/com/example/parques/BoardGameActivity.kt
+++ app/src/main/java/com/example/parques/BoardGameActivity.kt
@@ -17,8 +17,8 @@ import kotlin.random.Random
 class BoardGameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBoardGameBinding
     private lateinit var firebaseInstance: FirebaseInstance
-    private var id:Int = 0
-    private var par:Boolean = false
+    private var id: Int = 0
+    private var par: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBoardGameBinding.inflate(layoutInflater)
@@ -28,13 +28,12 @@ class BoardGameActivity : AppCompatActivity() {
         id = extra?.getInt("id")!!
 
         firebaseInstance = FirebaseInstance(this)
-
         initListener()
         title()
         enabledDice()
     }
 
-    private fun initListener(){
+    private fun initListener() {
         binding.btnCast.setOnClickListener {
             play()
         }
@@ -47,84 +46,87 @@ class BoardGameActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-    private fun title(){
+
+    private fun title() {
         if (id != null) {
-            if(id == 1){
+            if (id == 1) {
                 binding.Titulo.text = "Jugador 1"
-            }else if(id == 2){
+            } else if (id == 2) {
                 binding.Titulo.text = "Jugador 2"
             }
         }
     }
-    private fun dice(): Int{
-        var dado1:Int = Random.nextInt(1,7)
-        var dado2:Int = Random.nextInt(1,7)
 
+    private fun dice(): Int {
+        var dado1: Int = Random.nextInt(1, 7)
+        var dado2: Int = Random.nextInt(1, 7)
         binding.tvDado1.text = dado1.toString()
         binding.tvDado2.text = dado2.toString()
-
-        if (dado1 == dado2){
-            par = true
-        }else{
-            par = false
-        }
-
+        par = dado1 == dado2
         return dado2 + dado1
-
     }
 
-    private fun enabledDice(){
-        val postListener = object: ValueEventListener {
+    private fun enabledDice() {
+        val postListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val data = getCleanSnapshot(snapshot)
                 var aux = data?.second
-
-                if(aux!= null){
-                    if(aux.TurnoJugador == true && id == 1 || aux.TurnoJugador == false && id == 2){
-                        binding.btnCast.isEnabled = true
-                    }else{
-                        binding.btnCast.isEnabled = false
+                if (aux != null) {
+                    if (id == 1) {
+                        binding.btnCast.isEnabled = aux.TurnoJugador
+                    } else if (id == 2) {
+                        binding.btnCast.isEnabled = !aux.TurnoJugador
                     }
                 }
             }
             override fun onCancelled(error: DatabaseError) {
                 Log.i("Error onCancelled", error.details)
             }
-
         }
-
         firebaseInstance.setupDatabaseListener(postListener)
     }
 
-    private fun play(){
-        var recorrido: Int = dice()
+    private fun play() {
+        var recorrido: Int = dice() //Obteniendo el valor a recorrer del jugador
 
-        val postListener = object: ValueEventListener {
+        turn() //Habilitamos o deshabilitamos el boton para saber de que jugador es el turno
+
+    }
+
+    private fun pares(p: partida) {
+        if (par == true && id == 1 || par == true && id == 2) {
+            if (id == 1) {
+                binding.btnStartPlayer1.setBackgroundColor(resources.getColor(R.color.btn_color_player1))
+            } else if (id == 2) {
+                binding.btnStartPlayer2.setBackgroundColor(resources.getColor(R.color.btn_color_player2))
+            }
+        }
+    }
+
+    private fun turn() {
+        var prueba = partida()
+        val postListener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val data = getCleanSnapshot(snapshot)
                 var aux = data?.second
-
-                if(aux!= null){
-                    aux.TurnoJugador
-                    if(id == 1 && aux.TurnoJugador == true || id == 2 && aux.TurnoJugador == false){
-                        if (par == true && id == 1){
-                            binding.btn1.setBackgroundColor(resources.getColor(R.color.btn_color_player1))
-                        }
-                    }
+                if (aux != null) {
+                    prueba = aux
                 }
-
-
             }
             override fun onCancelled(error: DatabaseError) {
                 Log.i("Error onCancelled", error.details)
             }
-
         }
-
         firebaseInstance.setupDatabaseListener(postListener)
+
+        prueba.TurnoJugador = !prueba.TurnoJugador
+
+        firebaseInstance.writeOnFirebase(prueba)
+        pares(prueba)
+
     }
 
-    private fun color(){
+    private fun color() {
         var botonRojo = Button(this)
         botonRojo.setBackgroundColor(resources.getColor(R.color.btn_color_player1))
         val botonR = (botonRojo.background as? ColorDrawable)?.color
@@ -133,10 +135,10 @@ class BoardGameActivity : AppCompatActivity() {
         val botonA = (botonAmarillo.background as? ColorDrawable)?.color
     }
 
-    private fun getCleanSnapshot(snapshot: DataSnapshot): Pair<String,partida>?{
+    private fun getCleanSnapshot(snapshot: DataSnapshot): Pair<String, partida>? {
         val partidaKey = "1"
         val partidaValue = snapshot.getValue(partida::class.java)
 
-        return Pair(partidaKey!!,partidaValue!!)
+        return Pair(partidaKey!!, partidaValue!!)
     }
 }
